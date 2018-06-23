f2ee256dd66e9a75a401cc2b9bd281764b823783
casab <casabonelucas@hotmail.com>
2018-06-23 12:56:36 -0300

    Se agregan :
    - calculo de W, Ws, Wq, L, Lq para mms (falta añadir las ecuaciones correctas)

--- stadistic.py
+++ stadistic.py
@@ -1,4 +1,4 @@
-class Stadistic(object):
+class Statistics(object):
     def __init__(self, lbmda, mu, s, n, nq):
         self.lbmda = lbmda
         self.mu = mu
@@ -6,42 +6,83 @@ class Stadistic(object):
         self.n = n
         self.nq = nq
         self.p = 0
-        self.l = 0
+        self.len = 0
         self.lq = 0
+        self.w = 0
+        self.wq = 0
+        self.ws = 0
 
-    def set_intensity_trafic(self):
+    # p
+    def set_intensity_traffic(self):
         self.p = self.lbmda / self.mu
 
-    def get_intensity_trafic(self):
+    def get_intensity_traffic(self):
         return self.p
 
+    # L
     def set_client_quantity_system(self):
-        self.l = self.expectation(self.n)
+        if self.s > 1:
+            self.len = self.get_expectation(self.n)
+        else:
+            self.len = self.get_expectation(self.n)
 
     def get_client_quantity_system(self):
-        return self.l
+        return self.len
 
+    # LQ
     def set_client_quantity_queue(self):
-        self.lq = self.expectation(self.nq)
+        if self.s > 1:
+            self.lq = self.get_expectation(self.nq)
+        else:
+            self.lq = self.get_expectation(self.nq)
 
     def get_client_quantity_queue(self):
         return self.lq
 
-    def expectation(self, n):
+    # E
+    def get_expectation(self, n):
         expectation_summation = 0
         for x in range(0, n):
             expectation_summation += self.lbmda * self.probability_n(x)
         return expectation_summation
 
+    # Pn
     def probability_n(self, n):
         p0 = 1 / (1 + self.cn(n))
-        cn = self.p ** n
+        cn = self.get_intensity_traffic ** n
         pn = cn * p0
         return pn
 
-
+    # Cn
     def cn(self, n):
         cn_summation = 0
         for x in range(0, n):
-            cn_summation += self.p**x
+            cn_summation += self.get_intensity_traffic()**x
         return cn_summation
+
+    # W
+    def set_time_average_client_system(self):
+        if self.s > 1:
+            self.w = self.get_client_quantity_system() / self.lbmda
+        else:
+            self.w = self.get_client_quantity_system() / self.lbmda
+
+    def get_time_average_client_system(self):
+        return self.w
+
+    # Wq
+    def set_time_average_client_queue(self):
+        if self.s > 1:
+            self.wq = self.get_client_quantity_queue() / self.lbmda
+        else:
+            self.wq = self.get_client_quantity_queue() / self.lbmda
+
+    def get_time_average_client_queue(self):
+        return self.wq
+
+    # ws
+    def set_time_average_service_system(self):
+        self.ws = 1 / self.mu
+
+    def get_time_average_service_system(self):
+        return self.ws
